3bc918dc0ccde10af7b2e2eeaeaab988d7656cc4
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2024-12-18 15:15:22 +0530

    Generate random code from code-maker

---
+++ lib/CodeMaker.rb
@@ -0,0 +1,16 @@
+class CodeMaker
+  COLORS = ["R", "G", "B", "Y", "V", "P"]
+
+  attr_reader :code
+
+  def initialize
+    self.code = make_code
+  end
+
+  private
+  attr_writer :code
+
+  def make_code
+    COLORS.shuffle[0..4]
+  end
+end
